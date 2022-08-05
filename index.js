e0074d5f6ff93fad2ac7383bae2e5d7dd3cd8ef0
Luka Ilic <luka.ilic3052@gmail.com>
2022-08-06 00:52:15 +0200

    all

---
+++ index.js
@@ -0,0 +1,37 @@
+function openVideo() {
+    document.querySelector(".video__wrapper").style.display = "flex";
+    document.body.style.overflowY = "hidden";
+}
+
+function closeVideo() {
+    document.querySelector(".video__wrapper").style.display = "none";
+    document.body.style.overflowY = "scroll";
+}
+
+function load() {
+    if (window.innerWidth < 960) {
+        document.querySelector(" .javaImg").src = "./assetss/videoimg1.jpg";
+    }
+}
+
+function openSide() {
+    if (window.innerWidth < 960) {
+        document.querySelector(".phone__menu--wrapper").style.display = 'flex';
+
+        setTimeout(() => {
+            document.querySelector(".phone__menu--wrapper").style.opacity = '1';
+            document.querySelector(".phone__menu").style.left = 0;
+        }, 100);
+    }
+
+}
+
+function zclose() {
+    document.querySelector(".phone__menu").style.left = '-90%';
+    document.querySelector(".phone__menu--wrapper").style.opacity = '0';
+
+    setTimeout(() => {
+        document.querySelector(".phone__menu--wrapper").style.display = 'none';
+
+    }, 100);
+}
